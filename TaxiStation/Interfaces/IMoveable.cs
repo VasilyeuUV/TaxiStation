ccc0d695ee18e5c2e0c427051e121661d3cbaca5
VasilyeuUV <vasilyeu.uv@mail.ru>
2019-11-06 01:15:41 +0300

    Motion parameters

---
+++ TaxiStation/Interfaces/IMoveable.cs
@@ -0,0 +1,19 @@
+﻿namespace TaxiStation.Interfaces
+{
+    /// <summary>
+    /// The main speed parameters of the object to move (up, down, left, right, forward, backward)
+    /// </summary>
+    public interface IMoveable
+    {
+        ///// <summary>
+        ///// Сurrent object speed
+        ///// </summary>
+        //int CurrentSpeed { get; }
+
+        /// <summary>
+        /// Maximum object speed
+        /// </summary>
+        int MaxSpeed { get; }
+    
+    }
+}
---
+++ TaxiStation/Interfaces/ISelfPropelled.cs
@@ -0,0 +1,30 @@
+﻿namespace TaxiStation.Interfaces
+{
+    public interface ISelfPropelled : IMoveable
+    {
+
+        ///// <summary>
+        ///// Increase speed by one unit
+        ///// </summary>
+        //void IncreaseSpeed(int value = 1);
+
+        ///// <summary>
+        ///// Reduce speed by one unit
+        ///// </summary>
+        //void ReduceSpeed(int value = 1);
+
+
+        ///// <summary>
+        ///// Set current
+        ///// </summary>
+        ///// <param name="value"></param>
+        //void SetSpeed(int value);
+
+        ///// <summary>
+        ///// Stop the movement of an object
+        ///// </summary>
+        //void Stop();
+
+
+    }
+}
